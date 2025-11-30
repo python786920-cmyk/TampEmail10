b5936d0d90e21320c055fba245a167f79148b72e
python786920-cmyk <python786920@gmail.com>
2025-11-30 17:28:02 +0530

    Create config.js

---
+++ config.js
@@ -0,0 +1,6 @@
+// config.js
+module.exports = {
+  TELEGRAM_BOT_TOKEN: '8156606835:AAEUmklO-2Tx2AR1-gQ_7MP783QmX20ykBY', // yahan apna bot token daalo
+  DB_PATH: './data.sqlite',
+  POLL_INTERVAL_MS: 7000, // 7 sec - "realtime" notifications
+};
